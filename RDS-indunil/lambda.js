a6e8e1674aa1dbf70752a22442b9a0838226f26b
Indunil1992 <indunil@adroitlogic.com>
2019-07-23 11:03:33 +0530

    ko

--- RDS-indunil/lambda.js
+++ RDS-indunil/lambda.js
@@ -1,19 +1,4 @@
-let SL_AWS = require('slappforge-sdk-aws');
-let connectionManager = require('./ConnectionManager');
-const rds = new SL_AWS.RDS(connectionManager);
-
 exports.handler = function (event, context, callback) {
-
-
-    // You must always end/destroy the DB connection after it's used
-    rds.beginTransaction({
-        instanceIdentifier: 'InduInstance'
-    }, function (error, connection) {
-        if (error) {
-            throw error;
-        }
-    });
-
 
 
     callback(null, { "message": "Successfully executed" });
